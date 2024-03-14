8fcf0fa43fcc9d7454f4f19ad1072547336e30c2
gi3a <gizat.uteev@gmail.com>
2024-03-14 14:04:32 -0500

    display

---
+++ pages/display/menu/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Menu #{{ menuId }}:</h1>
+</template>
+<script setup>
+definePageMeta({
+    layout: "display",
+});
+const route = useRoute();
+const menuId = route.params.id; 
+</script>
--- pages/display/status.vue
+++ pages/display/status.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="flex w-full h-screen justify-center items-center">
-        <div class="flex flex-col w-1/2">
-            <h2 class="text-7xl font-bold mb-6 p-4">Готовятся</h2>
-            <div v-for="order in preparingOrders" :key="order.id" class="flex items-center p-4 text-5xl">
-                <span class="rounded bg-[#009FFF] py-2 px-6">{{ order.number }}</span>
-                <span class="text-[#98BFF5] p-2">{{ order.name }}</span>
+    <div class="status-display">
+        <div class="status-side">
+            <h2>Preparing</h2>
+            <div v-for="order in preparingOrders" :key="order.id" class="status-item">
+                <span class="status-id">{{ order.number }}</span>
+                <span class="status-name">{{ order.name }}</span>
             </div>
         </div>
-        <div class="flex flex-col">
-            <h2 class="text-7xl font-bold mb-6 p-4">Готовы</h2>
-            <div v-for="order in readyOrders" :key="order.id" class="flex items-center p-4 text-5xl">
-                <span class="rounded bg-[#009FFF] py-2 px-6">{{ order.number }}</span>
-                <span class=" p-2">{{ order.name }}</span>
+        <div class="status-side">
+            <h2>Ready</h2>
+            <div v-for="order in readyOrders" :key="order.id" class="status-item">
+                <span class="status-id">{{ order.number }}</span>
+                <span class="status-name">{{ order.name }}</span>
             </div>
         </div>
     </div>
@@ -22,21 +22,71 @@ export default {
     data() {
         return {
             preparingOrders: [
-                { id: 1, number: 42, name: 'Сударушка' },
-                { id: 2, number: 44, name: 'Дмитрий' },
-                { id: 3, number: 45, name: 'Даня' },
-                { id: 4, number: 46, name: 'Мария' },
-                { id: 5, number: 47, name: 'Дарима' },
+                { id: 1, number: 42, name: 'Genevra' },
+                { id: 2, number: 44, name: 'Alexey' },
+                { id: 3, number: 45, name: 'Abraham' },
+                { id: 4, number: 46, name: 'Maria' },
+                { id: 5, number: 47, name: 'Jose' },
             ],
             readyOrders: [
-                { id: 1, number: 33, name: 'Юлия' },
-                { id: 1, number: 34, name: 'Дмитрий' },
-                { id: 1, number: 35, name: 'вика' },
-                { id: 1, number: 36, name: 'Лика' },
-                { id: 1, number: 37, name: 'Александр' },
+                { id: 1, number: 33, name: 'Julia' },
+                { id: 1, number: 34, name: 'John' },
+                { id: 1, number: 35, name: 'Bill' },
+                { id: 1, number: 36, name: 'Ammie' },
+                { id: 1, number: 37, name: 'Anastasia' },
 
             ]
         }
     }
 }
 </script>
+<script setup>
+definePageMeta({
+    layout: "display",
+});
+</script>
+
+<style>
+.status-display {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    height: 95vh;
+    overflow: hidden;
+}
+
+.status-side {
+    display: grid;
+    grid-column: span 1;
+    gap: 1.5rem;
+    margin-left: 10rem;
+
+    &>h2 {
+        font-weight: bold;
+        font-size: 3rem;
+    }
+}
+
+.status-item {
+    display: grid;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+    align-items: center;
+    gap: 0.2rem;
+    font-size: 2rem;
+    font-weight: bold;
+}
+
+.status-id {
+    display: grid;
+    text-align: center;
+    padding: 1.5rem 0.5rem;
+    border-radius: 0.25rem;
+    background-color: rgb(0 159 255);
+    grid-column: span 1;
+}
+
+.status-name {
+    display: grid;
+    padding-left: 20px;
+    grid-column: span 4;
+}
+</style>
