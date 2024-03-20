e9f8d6d5bf196166064c709870ecaf157d391c45
gi3a <gizat.uteev@gmail.com>
2024-03-20 13:32:40 -0500

    profile and settings page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,9 @@
 <template>
   <nav class="nav-bar flex sticky top-2 left-0 right-0 z-40 rounded">
-    <NuxtLink to="/"> Magnifico Coffee </NuxtLink>
+    <NuxtLink to="/"> Test Logotype </NuxtLink>
     <a href="#" @click="handleProfileNavigation">Profile</a>
   </nav>
-  <LoginPopup
-    :isOpen="isLoginPopupOpen"
-    @update:isOpen="isLoginPopupOpen = $event"
-  />
+  <LoginPopup :isOpen="isLoginPopupOpen" @update:isOpen="isLoginPopupOpen = $event" />
 </template>
 
 <script setup>
@@ -32,7 +29,7 @@ const handleProfileNavigation = (event) => {
   justify-content: space-between;
   transition: 0.5s all;
 
-  & > a {
+  &>a {
     display: flex;
     justify-content: center;
     align-items: center;
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,19 +1,20 @@
 <template>
-  Профиль
-  <button
-    @click="logOut"
-    class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-  >
-    Log out
-  </button>
+  <div v-if="user">
+    <ProfileBar />
+    <HistoryBar />
+  </div>
 </template>
-<script setup>
-import { ref } from "vue";
-const supabase = useSupabaseClient();
-const router = useRouter();
 
-const logOut = async () => {
-  const { error } = await supabase.auth.signOut();
-  router.push("/");
-};
+<script setup>
+const router = useRouter();
+const user = useSupabaseUser();
+
+
+watchEffect(() => {
+  if (!user.value) {
+    router.push("/");
+  }
+});
+
+
 </script>
